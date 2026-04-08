3c4365ff54699ed1a480da9648cd6807a27e9b41
wsh-cc <1901592998@qq.com>
2026-04-08 21:59:29 +0800

    add new project

---
+++ program_UserManager/user_service.py
@@ -0,0 +1,75 @@
+from storage import load_users, save_users
+from logger import logger
+
+
+# 添加用户
+def add_user(name, age, email):
+    users = load_users()#程序第一次调用时，会返回一个空列表？
+
+    if not name:
+        raise ValueError("姓名不能为空")#如果name为空字符串，抛出ValueError异常，提示姓名不能为空
+
+    if age <= 0:
+        raise ValueError("年龄不合法")
+
+    user = {
+        "id": len(users) + 1,
+        "name": name,
+        "age": age,
+        "email": email
+        #可以用正则来检验邮箱的合法性，可升级
+    }
+
+    users.append(user)#
+    save_users(users)
+
+    logger.info(f"添加用户: {user}")#
+    return user
+
+
+# 查询全部
+def get_users():
+    return load_users()
+
+
+# 根据ID查询
+def get_user_by_id(user_id):
+    users = load_users()
+
+    for user in users:
+        if user["id"] == user_id:
+            return user
+
+    raise ValueError("用户不存在")
+
+
+# 修改用户
+def update_user(user_id, name, age, email):
+    users = load_users()
+
+    for user in users:
+        if user["id"] == user_id:
+            user["name"] = name
+            user["age"] = age
+            user["email"] = email
+
+            save_users(users)
+            logger.info(f"修改用户: {user}")
+            return user
+
+    raise ValueError("用户不存在")
+
+
+# 删除用户
+def delete_user(user_id):
+    users = load_users()
+
+    for user in users:
+        if user["id"] == user_id:
+            users.remove(user)
+            save_users(users)
+
+            logger.info(f"删除用户: {user}")
+            return
+
+    raise ValueError("用户不存在")
